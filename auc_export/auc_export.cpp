0c2495c79d903a752e80895aefd46b1af583f973
rigaya <rigaya34589@live.jp>
2025-09-06 22:46:24 +0900

    初期コミット。

---
+++ auc_export/auc_export.cpp
@@ -0,0 +1,86 @@
+﻿#include <Windows.h>
+#include <stdio.h>
+#pragma warning( push )
+#pragma warning( disable: 4706 )
+#include "..\auc.h"
+#pragma warning( pop )
+
+static char *USAGE = "auc_export.exe [#window] #plugin <filename>";
+
+//lsmashmuxerのダイアログは親が設定されない
+//タイトル名とプロセスIDから特定する
+static HWND getdlghwnd_muxer(HWND hwnd) {
+	HWND hwnd_result = NULL;
+	DWORD pid_main_window = 0;
+	GetWindowThreadProcessId(hwnd, &pid_main_window);
+
+	Sleep(100);
+	for (int i = 0; i < 20; i++) {
+		HWND hwndd = FindWindowEx(NULL, NULL, NULL, "lsmashmuxer");
+		if (NULL != hwndd) {
+			DWORD pid_window = 0;
+			GetWindowThreadProcessId(hwndd, &pid_window);
+			if (pid_main_window == pid_window) {
+				hwnd_result = hwndd;
+				break;
+			}
+		}
+		Sleep(100);
+	}
+	if (NULL == hwnd_result)
+		error_exit("could not get dialog window.");
+
+	Sleep(100);
+
+	return hwnd_result;
+}
+
+int main(int argc, char **argv) {
+	if (argc < 3 || argc > 4) error_exit(USAGE);
+	if (argv[argc - 2][0] < '0' || argv[argc - 2][0] > '9') error_exit(USAGE);
+
+	int num = atoi(argv[argc - 2]);
+
+	HWND hwnd = (argc == 4) ? gethwnd_str(argv[1]) : gethwnd();
+
+	//エクスポートプラグインをクリック
+	PostMessage(hwnd, WM_COMMAND, 15031 + num, 0);
+
+	//L-SMASH Works Muxerのダイアログを探してみる
+	HWND hwnd_muxer_dlg = getdlghwnd_muxer(hwnd);
+	if (NULL != hwnd_muxer_dlg) {
+		//Muxボタンを押す
+		HWND hwnd_mux_button = FindWindowEx(hwnd_muxer_dlg, NULL, "Button", "Mux");
+		PostMessage(hwnd_mux_button, WM_LBUTTONDOWN, MK_LBUTTON, 0);
+		PostMessage(hwnd_mux_button, WM_LBUTTONUP, MK_LBUTTON, 0);
+		Sleep(100);
+		if (IsWindow(hwnd_muxer_dlg) && GetFocus() == hwnd_mux_button) {
+			char buf[1024] = { 0 };
+			GetWindowText(hwnd_muxer_dlg, buf, _countof(buf));
+			if (0 == strcmp(buf, "lsmashmuxer")) {
+				PostMessage(hwnd_mux_button, WM_LBUTTONDOWN, MK_LBUTTON, 0);
+				PostMessage(hwnd_mux_button, WM_LBUTTONUP, MK_LBUTTON, 0);
+			}
+		}
+	}
+	//保存ダイアログを探す
+	HWND hwnd_savefile_dlg = getdlghwnd(hwnd);
+
+	//あとはaji様と同じように
+	HWND hwnd_save_button = FindWindowEx(hwnd_savefile_dlg, NULL, "Button", "保存(&S)");
+	HWND hwnd_comboboxex = FindWindowEx(hwnd_savefile_dlg, NULL, "ComboBoxEx32", NULL);
+	if (hwnd_comboboxex == NULL) {
+		hwnd_comboboxex = FindWindowEx(hwnd_savefile_dlg, NULL, "DUIViewWndClassName", NULL);
+		if (hwnd_comboboxex) hwnd_comboboxex = FindWindowEx(hwnd_comboboxex, NULL, "DirectUIHWND", NULL);
+		if (hwnd_comboboxex) hwnd_comboboxex = FindWindowEx(hwnd_comboboxex, NULL, "FloatNotifySink", NULL);
+	}
+	if (hwnd_save_button == NULL || hwnd_comboboxex == NULL) error_exit("Unexpected dialog.");
+	HWND hwnd_combobox  = FindWindowEx(hwnd_comboboxex, NULL, "ComboBox", NULL);
+	HWND hwnd_save_filename = FindWindowEx(hwnd_combobox, NULL, "Edit", NULL);
+	DeleteFile(argv[argc - 1]);
+	SendMessage(hwnd_save_filename, WM_SETTEXT, 0, (LPARAM)argv[argc - 1]);
+	SendMessage(hwnd_save_button, WM_LBUTTONDOWN, 0, 0);
+	SendMessage(hwnd_save_button, WM_LBUTTONUP, 0, 0);
+
+	return 0;
+}
---
+++ auc_wait_export/auc_wait_export.cpp
@@ -0,0 +1,65 @@
+﻿#include <stdio.h>
+#include <windows.h>
+#pragma warning( push )
+#pragma warning( disable: 4706 )
+#include "..\auc.h"
+#pragma warning( pop )
+
+static char *usage = "usage: auc_wait_export [#window]";
+
+static DWORD cpu_core_count() {
+	SYSTEM_INFO si;
+	GetSystemInfo(&si);
+	return si.dwNumberOfProcessors;
+}
+
+UINT64 get_elapsed_time_100ns(FILETIME fin, FILETIME start) {
+	return (((UINT64)fin.dwHighDateTime << 32) + fin.dwLowDateTime) - (((UINT64)start.dwHighDateTime << 32) + start.dwLowDateTime);
+}
+
+double get_process_cpu_usage(DWORD target_process_id, DWORD measure_time_ms) {
+	FILETIME creation_time;
+	FILETIME exit_time;
+	FILETIME kernel_time_start;
+	FILETIME kernel_time_fin;
+	FILETIME user_time_start;
+	FILETIME user_time_fin;
+
+	HANDLE h_process = OpenProcess(PROCESS_QUERY_INFORMATION, FALSE, target_process_id);
+
+	if (!GetProcessTimes(h_process, &creation_time, &exit_time, &kernel_time_start, &user_time_start))
+		return 0;
+
+	Sleep(measure_time_ms);
+	
+	if (!GetProcessTimes(h_process, &creation_time, &exit_time, &kernel_time_fin, &user_time_fin))
+		return 0;
+
+	CloseHandle(h_process);
+
+	UINT64 elapsed_time_100ns = get_elapsed_time_100ns(kernel_time_fin, kernel_time_start) + get_elapsed_time_100ns(user_time_fin, user_time_start);
+
+	return elapsed_time_100ns * 0.0001 / (double)measure_time_ms / cpu_core_count();
+}
+
+int main(int argc, char **argv) {
+	if (argc < 1 || argc > 2) error_exit(usage);
+
+	HWND hwnd = (argc == 2) ? gethwnd_str(argv[1]) : gethwnd();
+
+	SetPriorityClass(GetCurrentProcess(), IDLE_PRIORITY_CLASS);
+
+	DWORD pid_aviutl = 0;
+	GetWindowThreadProcessId(hwnd, &pid_aviutl);
+
+	for (int i = 0; i < 7; i++) {
+		if (0.0001 < get_process_cpu_usage(pid_aviutl, 500)) {
+			Sleep(5000);
+			i = -1;
+		} else {
+			Sleep(500);
+		}
+	}
+
+	return 0;
+}
